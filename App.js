7a34ff7173987c80c3433aac249d8f3bf887dc1b
RBAGAPOROJR <ronald.ghub@gmail.com>
2024-02-11 12:34:18 -0500

    [#1] Implementation of creating, manage, modify and withdrawal of task / plan
    - Implementation of function of creating, manage, modify and withdrawal of task.
    - Adding redux functions.
    - Additional components ( ButtonIcon / Spacer js ).
    - Implementation an Alert message function in creating and updating task.

--- App.js
+++ App.js
@@ -3,13 +3,17 @@ import { AppRegistry } from "react-native";
 import { name as appName } from "./app.json";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import {MD3LightTheme as DefaultTheme, PaperProvider} from "react-native-paper";
+import { Button, MD3LightTheme as DefaultTheme, PaperProvider } from "react-native-paper";
 import { FontAwesome, Feather, AntDesign } from '@expo/vector-icons';
 import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialBottomTabNavigator } from "react-native-paper/react-navigation";
 import Home from "./screens/Home";
 import Settings from "./screens/Settings";
 import Tasks from "./screens/Tasks";
+
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -24,27 +28,29 @@ const theme = {
 
 export default function App() {
   return (
-    <PaperProvider theme={theme}>
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" options={{
-            tabBarIcon: () => (
-              <FontAwesome name="home" size={24} color="black" />
-            ),
-          }} component={Home} />
-          <Tab.Screen name="Tasks" options={{
-            tabBarIcon: () => (
-              <FontAwesome name="tasks" size={24} color="black" />
-            ),
-          }} component={Tasks} />
-          <Tab.Screen name="Settings" options={{
-            tabBarIcon: () => (
-              <Feather name="settings" size={24}/>
-            ),
-          }} component={Settings} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </PaperProvider>
+    <Provider store={store}>
+      <PaperProvider theme={theme}>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Home" options={{
+              tabBarIcon: () => (
+                <FontAwesome name="home" size={24} color="black" />
+              ),
+            }} component={Home} />
+            <Tab.Screen name="Tasks" options={{
+              tabBarIcon: () => (
+                <FontAwesome name="tasks" size={24} color="black" />
+              ),
+            }} component={Tasks} />
+            <Tab.Screen name="Settings" options={{
+              tabBarIcon: () => (
+                <Feather name="settings" size={24} />
+              ),
+            }} component={Settings} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </Provider>
   );
 }
 
