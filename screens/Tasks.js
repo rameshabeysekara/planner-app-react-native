1137cc913af783c7ddf1eddd5a88b30b44225607
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2024-02-10 20:29:06 -0500

    added screen for tasks

---
+++ screens/Tasks.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Tasks = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Tasks Here!</Text>
+    </View>
+  );
+};
+
+export default Tasks;
